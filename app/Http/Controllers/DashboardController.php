50efa11de76c6bc135d41168f800d24e92d364da
SideeID <sandipratama164026@gmail.com>
2025-05-13 10:53:02 +0700

    refactor: Improve role-based view rendering in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\DataFeed;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -15,25 +12,12 @@ class DashboardController extends Controller
             $user = Auth::user();
 
             if ($user->role === 'admin') {
-                $dataFeed = new DataFeed();
                 return view('pages.admin.dashboard', compact('dataFeed'));
             } elseif ($user->role === 'petugas') {
-                $dataFeed = new DataFeed();
                 return view('pages.petugas.dashboard', compact('dataFeed'));
             } elseif ($user->role === 'nasabah') {
-                $dataFeed = new DataFeed();
                 return view('pages.nasabah.dashboard', compact('dataFeed'));
             }
         }
-    }
-
-    public function analytics()
-    {
-        return view('pages/dashboard/analytics');
-    }
-
-    public function fintech()
-    {
-        return view('pages/dashboard/fintech');
     }
 }
