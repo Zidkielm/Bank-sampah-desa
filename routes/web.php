f9c300534ef3c731e2ec65165c25314103586b2e
SideeID <sandipratama164026@gmail.com>
2025-05-13 08:58:48 +0700

    Add CRUD routes for managing waste types and enhance deposit routes for admin and petugas roles

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\NasabahController;
 use App\Http\Controllers\PetugasController;
+use App\Http\Controllers\WasteTypeController;
+use App\Http\Controllers\DepositController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,22 +35,45 @@ Route::get('/landing', function () {
 
 // role admin
 Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->group(function () {
-
-    Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
-
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/dashboard/data-nasabah', [AdminController::class, 'dataNasabah'])->name('data-nasabah');
-    Route::get('/dashboard/data-petugas', [AdminController::class, 'dataPetugas'])->name('data-petugas');
-    Route::get('/dashboard/data-sampah', [AdminController::class, 'dataSampah'])->name('data-sampah');
-    Route::get('/dashboard/setoran', [AdminController::class, 'setoran'])->name('setoran');
     Route::get('/dashboard/tarik-saldo', [AdminController::class, 'tarikSaldo'])->name('tarik-saldo');
     Route::get('/dashboard/iuran', [AdminController::class, 'iuran'])->name('iuran');
+
+    Route::get('/dashboard/setoran', [DepositController::class, 'index'])->name('setoran');
+    Route::post('/dashboard/setoran', [DepositController::class, 'store'])->name('deposit.store');
+    Route::get('/dashboard/setoran/{id}', [DepositController::class, 'show'])->name('deposit.show');
+    Route::get('/dashboard/waste-type-price/{id}', [DepositController::class, 'getWasteTypePrice'])->name('waste-type-price');
+    Route::post('/dashboard/setoran/report', [DepositController::class, 'generateReport'])->name('deposit.report');
+
+    Route::resource('dashboard/nasabah', NasabahController::class)->names([
+        'store' => 'admin.nasabah.store',
+        'update' => 'admin.nasabah.update',
+        'destroy' => 'admin.nasabah.destroy',
+        'index' => 'data-nasabah',
+    ])->except(['create', 'show', 'edit']);
+
+    Route::resource('dashboard/petugas', PetugasController::class)->names([
+        'store' => 'admin.petugas.store',
+        'update' => 'admin.petugas.update',
+        'destroy' => 'admin.petugas.destroy',
+        'index' => 'data-petugas',
+    ])->except(['create', 'show', 'edit']);
+
+    Route::resource('dashboard/sampah', WasteTypeController::class)->names([
+        'index' => 'data-sampah',
+        'store' => 'admin.waste-types.store',
+        'update' => 'admin.waste-types.update',
+        'destroy' => 'admin.waste-types.destroy',
+    ])->except(['create', 'show', 'edit']);
 });
 
 // role petugas
 Route::middleware(['auth:sanctum', 'verified', 'role:petugas'])->group(function () {
     Route::get('petugas/dashboard', [DashboardController::class, 'index'])->name('petugas.dashboard');
-    Route::get('petugas/dashboard/setoran', [PetugasController::class, 'setoran'])->name('petugas.setoran');
+    Route::get('petugas/dashboard/setoran', [DepositController::class, 'petugasIndex'])->name('petugas.setoran');
+    Route::post('petugas/dashboard/setoran', [DepositController::class, 'store'])->name('petugas.deposit.store');
+    Route::get('petugas/dashboard/setoran/{id}', [DepositController::class, 'show'])->name('petugas.deposit.show');
+    Route::get('petugas/dashboard/waste-type-price/{id}', [DepositController::class, 'getWasteTypePrice'])->name('petugas.waste-type-price');
     Route::get('petugas/dashboard/iuran', [PetugasController::class, 'iuran'])->name('petugas.iuran');
 });
 
