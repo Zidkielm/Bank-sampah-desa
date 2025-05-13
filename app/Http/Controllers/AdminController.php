0892e6499137dbd18ce131170f4337bb83ecd8a3
SideeID <sandipratama164026@gmail.com>
2025-05-13 08:55:33 +0700

    Remove unused methods for dataNasabah and dataPetugas from AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,20 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AdminController extends Controller
 {
-    public function dataNasabah()
-    {
-        return view('pages.admin.data-nasabah');
-    }
-
-    public function dataPetugas()
-    {
-        return view('pages.admin.data-petugas');
-    }
-
     public function dataSampah()
     {
         return view('pages.admin.data-sampah');
@@ -30,6 +18,7 @@ class AdminController extends Controller
     {
         return view('pages.admin.tarik-saldo');
     }
+
     public function iuran()
     {
         return view('pages.admin.iuran');
