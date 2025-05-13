8a18de38de9b779f18dd7aefcac574e5e51a20b7
SideeID <sandipratama164026@gmail.com>
2025-05-13 10:39:58 +0700

    Add HasFormattedMoney trait to Balance model and implement formatted amount accessor

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -2,17 +2,23 @@
 
 namespace App\Models;
 
+use App\Traits\HasFormattedMoney;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Balance extends Model
 {
-    use HasFactory;
+    use HasFactory, HasFormattedMoney;
 
     protected $fillable = [
         'user_id',
         'amount',
     ];
+
+    public function getFormattedAmountAttribute()
+    {
+        return $this->formatMoney($this->amount);
+    }
 
     public function user()
     {
