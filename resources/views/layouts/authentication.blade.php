c7ab9cd58c4af7f1cc2bed71cb66b493b7f66cc8
Zidkielm <santoniozons@gmail.com>
2025-05-16 10:03:56 +0700

    ganti foto tampilan auth

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -1,71 +1,75 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <script>
-            if (localStorage.getItem('dark-mode') === 'false' || !('dark-mode' in localStorage)) {
-                document.querySelector('html').classList.remove('dark');
-                document.querySelector('html').style.colorScheme = 'light';
-            } else {
-                document.querySelector('html').classList.add('dark');
-                document.querySelector('html').style.colorScheme = 'dark';
-            }
-        </script>
-    </head>
-    <body class="font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400">
+    <!-- Styles -->
+    @livewireStyles
 
-        <main class="bg-white dark:bg-gray-900">
+    <script>
+        if (localStorage.getItem('dark-mode') === 'false' || !('dark-mode' in localStorage)) {
+            document.querySelector('html').classList.remove('dark');
+            document.querySelector('html').style.colorScheme = 'light';
+        } else {
+            document.querySelector('html').classList.add('dark');
+            document.querySelector('html').style.colorScheme = 'dark';
+        }
+    </script>
+</head>
 
-            <div class="relative flex">
+<body class="font-inter bg-gray-100 text-gray-600 antialiased dark:bg-gray-900 dark:text-gray-400">
 
-                <!-- Content -->
-                <div class="w-full md:w-1/2">
+    <main class="bg-white dark:bg-gray-900">
 
-                    <div class="min-h-[100dvh] h-full flex flex-col after:flex-1">
+        <div class="relative flex">
 
-                        <!-- Header -->
-                        <div class="flex-1">
-                            <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-                                <!-- Logo -->
-                                <a class="block" href="/">
-                                    <img class="h-10 w-auto mr-3" src="{{ asset('images/logo.svg') }}" alt="Logo">
-                                </a>
-                            </div>
+            <!-- Content -->
+            <div class="w-full md:w-1/2">
+
+                <div class="flex h-full min-h-[100dvh] flex-col after:flex-1">
+
+                    <!-- Header -->
+                    <div class="flex-1">
+                        <div class="flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
+                            <!-- Logo -->
+                            <a class="block" href="/">
+                                <img class="mr-3 h-10 w-auto" src="{{ asset('images/logo.svg') }}" alt="Logo">
+                            </a>
                         </div>
+                    </div>
 
-                        <div class="max-w-sm mx-auto w-full px-4 py-8">
-                            {{ $slot }}
-                        </div>
-
+                    <div class="mx-auto w-full max-w-sm px-4 py-8">
+                        {{ $slot }}
                     </div>
 
                 </div>
 
-                <!-- Image -->
-                <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
-                    <img class="object-cover object-center w-full h-full" src="{{ asset('images/auth-image.jpg') }}" width="760" height="1024" alt="Authentication image" />
-                </div>
-
             </div>
 
-        </main>
+            <!-- Image -->
+            <div class="absolute bottom-0 right-0 top-0 hidden md:block md:w-1/2" aria-hidden="true">
+                <img class="h-full w-full object-cover object-center" src="{{ asset('images/tentang-kami-3.png') }}"
+                    width="760" height="1024" alt="Authentication image" />
+            </div>
 
-        @livewireScriptConfig
-    </body>
+        </div>
+
+    </main>
+
+    @livewireScriptConfig
+</body>
+
 </html>
